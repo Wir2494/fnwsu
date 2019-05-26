30e21008aaca448b46b2a6f4a9f42823ce436bea
Wir2494 <wirmatabadal@gmail.com>
2019-05-26 19:50:27 -0300

    Fixed the styles by turning the polygon classes into global ones and the rest of the styles scoped

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <Navbar />
-    <router-view/>
+    <div class="filler">
+      <Navbar />
+      <router-view/>
+    </div>
     <Footer />
   </div>
 </template>
@@ -25,5 +27,26 @@ export default {
   margin: 0;
   padding: 0;
   background-color: #EF4937;
+}
+.polygon-1{
+  clip-path: polygon(50% 0%, 100% 0, 100% 53%, 57% 100%, 0 100%, 0 0);
+  background-color: #F8E3DD;
+  height: 100vh;
+  width: 100%;
+  z-index: 5;
+}
+.polygon-2{
+  clip-path: polygon(0 0, 100% 68%, 100% 100%, 0 100%);
+  background-color: #F8E3DD;
+  height: 150vh;
+  width: 100%;
+  z-index: 1;
+}
+.wrapper{
+    width: 90.7%;
+    margin: 0 auto;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -68,28 +68,7 @@ export default {
 }
 </script>
 
-<style>
-.polygon-1{
-  clip-path: polygon(50% 0%, 100% 0, 100% 53%, 57% 100%, 0 100%, 0 0);
-  background-color: #F8E3DD;
-  height: 100vh;
-  width: 100%;
-  z-index: 5;
-}
-.polygon-2{
-  clip-path: polygon(0 0, 100% 68%, 100% 100%, 0 100%);
-  background-color: #F8E3DD;
-  height: 150vh;
-  width: 100%;
-  z-index: 1;
-}
-.wrapper{
-    width: 90.7%;
-    margin: 0 auto;
-    height: 100vh;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-}
+<style scoped>
 .wrapper-small{
     height: 40vh;
 }
@@ -217,5 +196,4 @@ button{
     color: #F8A88F;
     
 }
-
 </style>
